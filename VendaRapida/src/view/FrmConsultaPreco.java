9a474a76ed3f53ae8db31ed89dab5db24f9f8c95
reiinoldo <reiinoldo@gmail.com>
2013-06-29 23:41:32 +0000

    Tela de Consulta de Preços

--- VendaRapida/src/view/FrmConsultaPreco.java
+++ VendaRapida/src/view/FrmConsultaPreco.java
@@ -32,11 +32,20 @@ public class FrmConsultaPreco extends javax.swing.JDialog {
         initComponents();
         setLocationRelativeTo(null);
         
-        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("+"), "plussCount");  
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('+'), "plussCount");  
         getRootPane().getActionMap().put("plussCount", new AbstractAction("plussCount") {     
             // The next two lines should be in one line              
             public void actionPerformed(ActionEvent evt) {
                 btMaisActionPerformed(evt);
+            }  
+        }  
+        );
+        
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('-'), "plussCount");  
+        getRootPane().getActionMap().put("plussCount", new AbstractAction("plussCount") {     
+            // The next two lines should be in one line              
+            public void actionPerformed(ActionEvent evt) {
+                btMenosActionPerformed(evt);
             }  
         }  
         );
@@ -245,6 +254,8 @@ public class FrmConsultaPreco extends javax.swing.JDialog {
 
     private void edQuantidadeFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_edQuantidadeFocusLost
         // TODO add your handling code here:
+        if(edQuantidade.getText().isEmpty())
+            edQuantidade.setText("1");
         calcular(0);
     }//GEN-LAST:event_edQuantidadeFocusLost
 
@@ -256,47 +267,6 @@ public class FrmConsultaPreco extends javax.swing.JDialog {
  
     }//GEN-LAST:event_btMaisKeyTyped
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmConsultaPreco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmConsultaPreco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmConsultaPreco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmConsultaPreco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                FrmConsultaPreco dialog = new FrmConsultaPreco(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btMais;
     private javax.swing.JButton btMenos;
