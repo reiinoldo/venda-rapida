f19615e16b6351ff64e7bbe677baafd6ae889b9c
maicon.gerardi@gmail.com <maicon.gerardi@gmail.com@e1e1090e-fb2e-099b-ad60-0ab0a372caff>
2013-06-30 17:20:57 +0000

    alteracoes na tela inicial

--- trunk/VendaRapida/src/model/Venda.java
+++ trunk/VendaRapida/src/model/Venda.java
@@ -6,9 +6,9 @@ import java.util.Date;
 import java.util.List;
 
 public class Venda {
-    
+
     public enum TipoDesconto {
-        
+
         PORCENTAGEM, DINHEIRO;
     }
     public static final String TABELA_VENDA = "vendarapida.venda";
@@ -25,35 +25,35 @@ public class Venda {
     public static final String CAMPO_LOGINUSUARIO = "venda.loginusuario";
     private String loginUsuario;
     private List<Item> items;
-    
+
     public List<Item> getItems() {
         return items;
     }
-    
+
     public Venda() {
         items = new ArrayList<Item>();
     }
-    
+
     public void setItems(List<Item> items) {
         this.items = items;
     }
-    
+
     public void addItem(Item item) {
         this.items.add(item);
     }
-    
+
     public void removerItem(Item item) {
         this.items.remove(item);
     }
-    
+
     public void removerItem(int indexItem) {
         this.items.remove(indexItem);
     }
-    
+
     public Item getItem(int index) {
         return items.get(index);
     }
-    
+
     public int getQuantidadeItens() {
         int qtd = 0;
         for (Item item : items) {
@@ -61,88 +61,89 @@ public class Venda {
         }
         return qtd;
     }
-    
-    public double getValorTotal(TipoDesconto tipoDesconto) throws RegraNegocioException {
-        double valorTotalSemDesconto = 0;
-        for (Item item : items) {
-            valorTotalSemDesconto += item.getValor() * item.getQuantidade();
-        }
-        
-        double valorComDesconto = 0;
-        switch (tipoDesconto) {
-            case PORCENTAGEM:
-                if (desconto <= 100) {
-                    double descontoTotal = (valorTotalSemDesconto * desconto) / 100;
-                    valorComDesconto = valorTotalSemDesconto - descontoTotal;
-                } else {
-                    throw new RegraNegocioException("Você não pode dar um desconto maior que 100%.");
-                }
-                break;
-            case DINHEIRO:
-                if (desconto < valorTotalSemDesconto) {
-                    valorComDesconto = valorTotalSemDesconto - desconto;
-                } else {
-                    throw new RegraNegocioException("Você não pode dar um desconto maior que o valor da compra.");
-                }
-                break;
-        }
-        setDesconto(valorComDesconto);
-        return valorComDesconto;
-    }
-    
-    public double getValorTotal() {
+
+    public double getValorTotalComDesconto() {
         double valorTotalSemDesconto = 0;
         for (Item item : items) {
             valorTotalSemDesconto += item.getValor() * item.getQuantidade();
         }
         return valorTotalSemDesconto - desconto;
     }
-    
+
+    public double getValorTotalSemDesconto() {
+        double valorTotalSemDesconto = 0;
+        for (Item item : items) {
+            valorTotalSemDesconto += item.getValor() * item.getQuantidade();
+        }
+        return valorTotalSemDesconto;
+    }
+
     public int getIdCliente() {
         return idCliente;
     }
-    
+
     public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
     }
-    
+
     public String getCodigoPagSeguro() {
         return codigoPagSeguro;
     }
-    
+
     public void setCodigoPagSeguro(String codigoPagSeguro) {
         this.codigoPagSeguro = codigoPagSeguro;
     }
-    
+
     public int getCodigoVenda() {
         return codigoVenda;
     }
-    
+
     public void setCodigoVenda(int codigoVenda) {
         this.codigoVenda = codigoVenda;
     }
-    
+
     public Date getDataVenda() {
         return dataVenda;
     }
-    
+
     public void setDataVenda(Date dataVenda) {
         this.dataVenda = dataVenda;
     }
-    
+
     public double getDesconto() {
-        
         return desconto;
     }
-    
+
     public void setDesconto(double desconto) {
         this.desconto = desconto;
     }
-    
+
+    public void setDesconto(TipoDesconto tipo, double desconto) throws RegraNegocioException {
+        double valorTotal = getValorTotalSemDesconto();
+        double descontoTotal = 0;
+        switch (tipo) {
+            case PORCENTAGEM:
+                if (desconto <= 100) {
+                    descontoTotal = (valorTotal * desconto) / 100;
+                } else {
+                    throw new RegraNegocioException("Você não pode dar um desconto maior que 100%.");
+                }
+                break;
+            case DINHEIRO:
+                if (desconto <= valorTotal) {
+                    descontoTotal = desconto;
+                } else {
+                    throw new RegraNegocioException("Você não pode dar um desconto maior que o valor da compra.");
+                }
+                break;
+        }
+        this.desconto = descontoTotal;
+    }
+
     public String getLoginUsuario() {
         return loginUsuario;
     }
-    
+
     public void setLoginUsuario(String loginUsuario) {
         this.loginUsuario = loginUsuario;
     }
