fa9185af1aa8cb122bb5aa1c295dfe8a232567b7
maicon.gerardi@gmail.com <maicon.gerardi@gmail.com@e1e1090e-fb2e-099b-ad60-0ab0a372caff>
2013-06-30 00:16:01 +0000

    alterações no form de simulação de venda.

--- trunk/VendaRapida/src/model/Venda.java
+++ trunk/VendaRapida/src/model/Venda.java
@@ -1,10 +1,11 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Venda {
-    
+
     public static final String TABELA_VENDA = "vendarapida.venda";
     public static final String CAMPO_CODIGOVENDA = "venda.codigovenda";
     private int codigoVenda;
@@ -19,71 +20,75 @@ public class Venda {
     public static final String CAMPO_LOGINUSUARIO = "venda.loginusuario";
     private String loginUsuario;
     private List<Item> items;
-    
+
     public List<Item> getItems() {
         return items;
     }
-    
+
+    public Venda() {
+        items = new ArrayList<Item>();
+    }
+
     public void setItems(List<Item> items) {
         this.items = items;
     }
-    
+
     public void addItem(Item item) {
         this.items.add(item);
     }
-    
+
     public void removerItem(Item item) {
         this.items.remove(item);
     }
-    
+
     public int getIdCliente() {
         return idCliente;
     }
-    
+
     public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
     }
-    
+
     public String getCodigoPagSeguro() {
         return codigoPagSeguro;
     }
-    
+
     public void setCodigoPagSeguro(String codigoPagSeguro) {
         this.codigoPagSeguro = codigoPagSeguro;
     }
-    
+
     public int getCodigoVenda() {
         return codigoVenda;
     }
-    
+
     public void setCodigoVenda(int codigoVenda) {
         this.codigoVenda = codigoVenda;
     }
-    
+
     public Date getDataVenda() {
         return dataVenda;
     }
-    
+
     public void setDataVenda(Date dataVenda) {
         this.dataVenda = dataVenda;
     }
-    
+
     public double getDesconto() {
         return desconto;
     }
-    
+
     public void setDesconto(double desconto) {
         this.desconto = desconto;
     }
-    
+
     public String getLoginUsuario() {
         return loginUsuario;
     }
-    
+
     public void setLoginUsuario(String loginUsuario) {
         this.loginUsuario = loginUsuario;
     }
-    
+
     public double getValor() {
         double valor = 0;
         for (int i = 0; i < getItems().size(); i++) {
